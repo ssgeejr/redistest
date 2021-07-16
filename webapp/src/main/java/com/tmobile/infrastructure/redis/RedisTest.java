1bd0d9be50b4904267263f2db1210f7d7fd6ba79
steven gee <ioexcept@gmail.com>
2021-07-16 09:21:39 -0500

    test worked

--- webapp/src/main/java/com/tmobile/infrastructure/redis/RedisTest.java
+++ webapp/src/main/java/com/tmobile/infrastructure/redis/RedisTest.java
@@ -5,7 +5,7 @@ import redis.clients.jedis.Jedis;
 public class RedisTest { 
    public static void main(String[] args) { 
       //Connecting to Redis server on localhost 
-      Jedis jedis = new Jedis("localhost"); 
+      Jedis jedis = new Jedis("redistest"); 
       System.out.println("Connection to server sucessfully"); 
       //check whether server is running or not 
       System.out.println("Server is running: "+jedis.ping()); 

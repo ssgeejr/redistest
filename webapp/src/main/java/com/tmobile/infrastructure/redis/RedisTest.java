204fa4dc2c461b0583e17416926ce4d88e90261e
Steven Gee <ioexcept@gmail.com>
2021-07-16 13:54:49 +0000

    ready to go

--- webapp/src/main/java/com/tmobile/infrastructure/redis/RedisTest.java
+++ webapp/src/main/java/com/tmobile/infrastructure/redis/RedisTest.java
@@ -2,7 +2,7 @@ package com.tmobile.infrastructure.redis;
 
 import redis.clients.jedis.Jedis; 
 
-public class RedisJava { 
+public class RedisTest { 
    public static void main(String[] args) { 
       //Connecting to Redis server on localhost 
       Jedis jedis = new Jedis("localhost"); 

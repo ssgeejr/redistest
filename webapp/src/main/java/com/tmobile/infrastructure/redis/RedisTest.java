1d95456a9ea07f8378f8b6534d0893b63c9fd692
Steven Gee <ioexcept@gmail.com>
2021-07-16 13:23:56 +0000

    adding java files

---
+++ webapp/src/main/java/com/tmobile/infrastructure/redis/RedisTest.java
@@ -0,0 +1,13 @@
+package com.tmobile.infrastructure.redis;
+
+import redis.clients.jedis.Jedis; 
+
+public class RedisJava { 
+   public static void main(String[] args) { 
+      //Connecting to Redis server on localhost 
+      Jedis jedis = new Jedis("localhost"); 
+      System.out.println("Connection to server sucessfully"); 
+      //check whether server is running or not 
+      System.out.println("Server is running: "+jedis.ping()); 
+   } 
+} 

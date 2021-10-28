5fa1456b691942a7a269a95297148bc35a73b8c7
steven gee <ioexcept@gmail.com>
2021-10-28 11:11:21 -0500

    testing localhost call

--- cluster/webdemo/src/main/java/com/tmobile/infrastructure/redis/TestClusterConnectivity.java
+++ cluster/webdemo/src/main/java/com/tmobile/infrastructure/redis/TestClusterConnectivity.java
@@ -1,16 +1,110 @@
 package com.tmobile.infrastructure.redis;
 
+import java.util.*;
+import java.util.Map.Entry;
+
+import redis.clients.jedis.*;
+
 import org.redisson.*;
 import org.redisson.api.*;
 import org.redisson.config.*;
 
+import net.bytebuddy.asm.Advice.This;
+
 public class TestClusterConnectivity {
 
     public static void main( String[] args ) {
+    	new TestClusterConnectivity().JedisTestAlpha();
+    }
+    
+	public void ClusterTest() {
+    	try {
+    		
+//    		--these aren't working because the cluser is set to localhost IP's .. the cluster will need to be rebuilt to support public IP connectivity
+	
+    		 JedisCluster jedisCluster = new JedisCluster(new HostAndPort("localhost", 12000));
+    		 String key = "testkey";
+    		 jedisCluster.set(key,"random-value");
+    		 
+    		 Thread.sleep(2000);
+    		 System.out.println("Fetched Value for '"  + key + "': " + jedisCluster.get(key));
+    		 
+//    		 Set<Entry<String, JedisPool>> nodes =  jedisCluster.getClusterNodes().entrySet();
+//    		 jedisCluster.set("cluster","istrue");
+    		 
+//    		 jedisCluster.ping
+    	}catch(Exception ex) {
+    		ex.printStackTrace();
+    	}
+    }
+    		
+	public void JedisTestAlpha() {
+    	try {
+    		
+//    		--these aren't working because the cluser is set to localhost IP's .. the cluster will need to be rebuilt to support public IP connectivity
+	
+//    		 JedisCluster jedisCluster = new JedisCluster(new HostAndPort("redistest", 12000));
+    		 Jedis jedis = new Jedis("redistest", 12000);
+    		 String key = "testkey";
+    		 jedis.set(key,"random-value");
+    		 
+    		 Thread.sleep(2000);
+    		 jedis = new Jedis("redistest", 12002);
+    		 System.out.println("Fetched Value for '"  + key + "': " + jedis.get(key));
+    		 
+//    		 Set<Entry<String, JedisPool>> nodes =  jedisCluster.getClusterNodes().entrySet();
+//    		 jedisCluster.set("cluster","istrue");
+    		 
+//    		 jedisCluster.ping
+    	}catch(Exception ex) {
+    		ex.printStackTrace();
+    	}
+    }
+    public void JedisTestBeta() {
+    	try {
+    	
+    		Set<HostAndPort> jedisClusterNode = new HashSet<HostAndPort>();
+    		
+    		jedisClusterNode.add(new HostAndPort("redistest", 12000));
+    		JedisPoolConfig cfg = new JedisPoolConfig();
+    		cfg.setMaxTotal(16);
+    		cfg.setMaxIdle(8);
+    		cfg.setMaxWaitMillis(10000);
+    		cfg.setTestOnBorrow(true); 
+    		
+//    		JedisCluster jc = new JedisCluster(jedisClusterNode);
+    		JedisCluster jc = new JedisCluster(jedisClusterNode, 10000, 1, cfg);
+    		
+    		
+    		
+//    		   public void setKeyInRedis(String key, String value) {
+//    			      getRedisCluster.set(key, value);
+//    			   }
+//
+//    			   public String getValueByKey(String key) {
+//    			      return (String) getRedisCluster.get(key);
+    		
+//    		jc.set("foo", "bar");
+//    		String value = jc.get("foo");
+    		
+    		
+//    	    System.out.println(value);
+//    	    try {
+//    	        jc.close();
+//    	    } catch (Exception e) {
+//    	        e.printStackTrace();
+//    	    }
+    	}catch(Exception ex) {
+    		ex.printStackTrace();
+    	}
+    }
+    	
+   public void RedissonTest() {
+    	
 	try{
-	Config config = new Config();
+		Config config = new Config();
         //config.useClusterServers().addNodeAddress("redis://localhost:12000", "redis://localhost:12002");
-	config.useClusterServers().setScanInterval(2000).addNodeAddress("redis://localhost:9443", "redis://localhost:9445"); 
+		config.useClusterServers().setScanInterval(2000).addNodeAddress("redis://redistest:12000", "redis://redistest:12002"); 
         RedissonClient redisson = Redisson.create(config);
 
         // operations with Redis based Lock
--- cluster/webdemo/src/main/java/com/tmobile/infrastructure/redis/ValidateClusterOnline.java
+++ cluster/webdemo/src/main/java/com/tmobile/infrastructure/redis/ValidateClusterOnline.java
@@ -6,11 +6,11 @@ public class ValidateClusterOnline {
 
     public static void main( String[] args ) {
         System.out.println( "Hello World!" );
-	Jedis jedis = new Jedis("localhost", 12000);
+        Jedis jedis = new Jedis("redistest", 12000);
         System.out.println("Connection to server sucessfully");
         //check whether server is running or not
         System.out.println("Cluster-1 is running: [" + jedis.ping() + "]");
-	jedis = new Jedis("localhost", 12002);
+	jedis = new Jedis("redistest", 12002);
         System.out.println("Cluster-2 is running: [" + jedis.ping() + "]");
     }
 }

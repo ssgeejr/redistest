07ea687f0e4652e754382d27cf68fe6667eae4a4
Steven Gee <ioexcept@gmail.com>
2021-10-28 13:40:45 +0000

    adding redis cluster test

---
+++ cluster/webdemo/src/main/java/com/tmobile/infrastructure/redis/RedisClusterTest.java
@@ -0,0 +1,10 @@
+package com.tmobile.infrastructure.redis;
+
+import redis.clients.jedis.Jedis;
+
+public class RedisClusterTest {
+
+    public static void main( String[] args ) {
+        System.out.println( "Hello World!" );
+    }
+}
